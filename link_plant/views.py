1661417e4e0cff65a73d6ff6d75b3529d56a2f2e
PaaSam <qaugustine08@gmail.com>
2024-07-14 15:10:27 +0000

    added a delete functionality using a delete view

--- link_plant/views.py
+++ link_plant/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from .models import Profile, Link
 
@@ -16,4 +16,8 @@ class LinkCreateView(CreateView):
 class LinkUpdateView(UpdateView):
     model = Link
     fields = ['text', 'url']
+    success_url = reverse_lazy('link-list')
+
+class LinkDeleteView(DeleteView):
+    model = Link
     success_url = reverse_lazy('link-list')
