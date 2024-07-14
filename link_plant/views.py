c16fd2b450a11b7a8ad5b9ebeffb5b27b241cfca
PaaSam <qaugustine08@gmail.com>
2024-07-14 16:05:03 +0000

    built the profile page to show links of users

--- link_plant/views.py
+++ link_plant/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
@@ -21,3 +21,12 @@ class LinkUpdateView(UpdateView):
 class LinkDeleteView(DeleteView):
     model = Link
     success_url = reverse_lazy('link-list')
+
+def profile_view(request, profile_slug):
+    profile = get_object_or_404(Profile,slug=profile_slug)
+    links = profile.links.all()
+    context = {
+        'profile': profile,
+        'links': links
+    }
+    return render(request, 'link_plant/profile.html', context)
